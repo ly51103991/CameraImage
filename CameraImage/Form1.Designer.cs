1c7b82a71a5c6b40e3796b9899b8c7b67e83ce58
毅 罗 <1003620101@qq.com>
2020-06-17 09:32:06 +0800

    1.8

---
+++ CameraImage/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace CameraImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TrueNum = new System.Windows.Forms.Label();
+            this.WrongNum = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.allNum = new System.Windows.Forms.Label();
+            this.btnAddModel = new System.Windows.Forms.Button();
+            this.modelList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkOnlineTimer = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.camStatus = new System.Windows.Forms.Label();
+            this.timeCam = new System.Windows.Forms.Label();
+            this.lazyTime = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(100, 49);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(344, 271);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(344, 271);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "开启";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.RealTimeSnap_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(220, 449);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 40);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TrueNum
+            // 
+            this.TrueNum.AutoSize = true;
+            this.TrueNum.Location = new System.Drawing.Point(156, 424);
+            this.TrueNum.Name = "TrueNum";
+            this.TrueNum.Size = new System.Drawing.Size(11, 12);
+            this.TrueNum.TabIndex = 8;
+            this.TrueNum.Text = "0";
+            // 
+            // WrongNum
+            // 
+            this.WrongNum.AutoSize = true;
+            this.WrongNum.Location = new System.Drawing.Point(263, 424);
+            this.WrongNum.Name = "WrongNum";
+            this.WrongNum.Size = new System.Drawing.Size(11, 12);
+            this.WrongNum.TabIndex = 9;
+            this.WrongNum.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "正确：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 424);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "错误：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(334, 424);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "一共：";
+            // 
+            // allNum
+            // 
+            this.allNum.AutoSize = true;
+            this.allNum.Location = new System.Drawing.Point(381, 424);
+            this.allNum.Name = "allNum";
+            this.allNum.Size = new System.Drawing.Size(11, 12);
+            this.allNum.TabIndex = 13;
+            this.allNum.Text = "0";
+            // 
+            // btnAddModel
+            // 
+            this.btnAddModel.Location = new System.Drawing.Point(336, 380);
+            this.btnAddModel.Name = "btnAddModel";
+            this.btnAddModel.Size = new System.Drawing.Size(75, 23);
+            this.btnAddModel.TabIndex = 14;
+            this.btnAddModel.Text = "添加模板";
+            this.btnAddModel.UseVisualStyleBackColor = true;
+            this.btnAddModel.Click += new System.EventHandler(this.btnAddModel_Click);
+            // 
+            // modelList
+            // 
+            this.modelList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modelList.FormattingEnabled = true;
+            this.modelList.Location = new System.Drawing.Point(223, 335);
+            this.modelList.Name = "modelList";
+            this.modelList.Size = new System.Drawing.Size(121, 20);
+            this.modelList.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "模板：";
+            // 
+            // checkOnlineTimer
+            // 
+            this.checkOnlineTimer.Interval = 3000;
+            this.checkOnlineTimer.Tick += new System.EventHandler(this.checkOnlineTimer_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "相机状态:";
+            // 
+            // camStatus
+            // 
+            this.camStatus.AutoSize = true;
+            this.camStatus.ForeColor = System.Drawing.Color.Green;
+            this.camStatus.Location = new System.Drawing.Point(208, 18);
+            this.camStatus.Name = "camStatus";
+            this.camStatus.Size = new System.Drawing.Size(29, 12);
+            this.camStatus.TabIndex = 19;
+            this.camStatus.Text = "上线";
+            // 
+            // timeCam
+            // 
+            this.timeCam.AutoSize = true;
+            this.timeCam.Location = new System.Drawing.Point(282, 18);
+            this.timeCam.Name = "timeCam";
+            this.timeCam.Size = new System.Drawing.Size(71, 12);
+            this.timeCam.TabIndex = 20;
+            this.timeCam.Text = "延时(毫秒):";
+            // 
+            // lazyTime
+            // 
+            this.lazyTime.Location = new System.Drawing.Point(359, 12);
+            this.lazyTime.Name = "lazyTime";
+            this.lazyTime.Size = new System.Drawing.Size(52, 21);
+            this.lazyTime.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(517, 528);
+            this.Controls.Add(this.lazyTime);
+            this.Controls.Add(this.timeCam);
+            this.Controls.Add(this.camStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modelList);
+            this.Controls.Add(this.btnAddModel);
+            this.Controls.Add(this.allNum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WrongNum);
+            this.Controls.Add(this.TrueNum);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.hWindowControl1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TrueNum;
+        private System.Windows.Forms.Label WrongNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label allNum;
+        private System.Windows.Forms.Button btnAddModel;
+        private System.Windows.Forms.ComboBox modelList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer checkOnlineTimer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label camStatus;
+        private System.Windows.Forms.Label timeCam;
+        private System.Windows.Forms.TextBox lazyTime;
+    }
+}
+
---
+++ CameraImage/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using HalconDotNet;
+using System.IO;
+using Microsoft.VisualBasic;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using MVSDK;
+namespace CameraImage
+{
+    public partial class Form1 : Form
+    {
+        public  HTuple hv_AcqHandle = null;
+        public HObject ho_image = null;
+        HalconAPI.HFramegrabberCallback delegateCallback;
+        int a = 0, b = 0, c = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            delegateCallback = takeCameraOne;
+            updateList();
+            /*HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "TriggerSource", "Line1");
+            //下面设置连续采集，上升沿触发，曝光模式等
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "AcquisitionMode", "Continuous");
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "TriggerSelector", "FrameStart");
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "TriggerActivation", "RisingEdge");
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "ExposureMode", "Timed");
+            //设置曝光时间
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "ExposureTime", 80000.0);
+            //下面为设置用不超时
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "grab_timeout", -1);*/
+            // IntPtr ptr = Marshal.GetFunctionPointerForDelegate(delegateCallback);//取回调函数的地址
+            //IntPtr ptr1 = GCHandle.Alloc(test, GCHandleType.Pinned).AddrOfPinnedObject();//取test变量的地址
+            // HOperatorSet.SetFramegrabberCallback(hv_AcqHandle, "transfer_end", ptr, ptr1);//注册回调函数
+            checkCamera();
+        }
+
+        void updateList()
+        {
+            modelList.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo("f:modelFiles/");
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                modelList.Items.Add(subDir.Name);
+            }
+            if (subDirs.Length!= 0) {
+            modelList.SelectedIndex = 0;
+                button2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+                MessageBox.Show("无模板可用 请添加模板！");
+            }
+        }
+
+        void checkCamera()
+        {
+            try
+            {
+                HOperatorSet.OpenFramegrabber("MindVision17_X64", 1, 1, 0, 0, 0, 0, "progressive",
+                 8, "Gray", -1, "false", "auto", "oufang", 0, -1, out hv_AcqHandle);
+                checkOnlineTimer.Enabled = true;
+                checkOnlineTimer.Start();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("未检测到相机！");
+                this.Close();
+            }
+        }
+        private void checkOnlineTimer_Tick(object sender, EventArgs e)
+        {
+            //HTuple newHandle=hv_AcqHandle.H.TupleIsValidHandle();
+            HTuple newHandle=  hv_AcqHandle.TupleIsNumber();
+            CameraSdkStatus status = MvApi.CameraConnectTest(newHandle);
+            if (status != 0)
+            {
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "trigger_mode", 1);
+                camStatus.Text = "相机已掉线 正在重连中...";
+                camStatus.ForeColor = Color.Red;
+                button2.Text = "开启";
+                modelList.Enabled = true;
+                btnAddModel.Enabled = false ;
+                button2.Enabled = false;
+                MvApi.CameraReConnect(newHandle);             
+            }else {
+                camStatus.Text = "上线";
+                camStatus.ForeColor = Color.Green;
+                button2.Enabled = true;
+                if (button2.Text == "开启") {
+                btnAddModel.Enabled = true; }
+                }
+            }
+        void returnNum()
+        {
+            TrueNum.Text = "0";WrongNum.Text = "0";allNum.Text = "0";
+            a = 0; b = 0; c =0;
+            pictureBox1.BackColor = Color.WhiteSmoke;
+        }
+        private int test = 1;//随便定义的一个变量，后面会取其地址带入回调函数的user_context
+
+        public int takeCameraOne(IntPtr handle,IntPtr context,IntPtr user_context)
+        {
+            try
+            {
+                HOperatorSet.GrabImage(out ho_image, hv_AcqHandle);
+                if (this.hWindowControl1.InvokeRequired)
+                {
+                    this.Invoke(new MethodInvoker(() => {
+                        if (ho_image != null) {
+                        checkModel(ho_image);
+                        }
+                    }));//把图像显示出来（这里是委托方式显示)
+                }
+                else
+                {
+                   // checkModel(ho_image);
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return -1;
+            }
+        }
+        private void RealTimeSnap_Click(object sender, EventArgs e)
+        {
+            int lTime = 0;
+            try
+            {
+                if(Int32.Parse(lazyTime.Text)>0){ lTime = Int32.Parse(lazyTime.Text); }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("延迟设置错误！请输入正整数：");
+                return;
+            }
+            if (button2.Text == "开启")
+            {                
+               // MvApi.CameraSetExtTrigSignalType(hv_AcqHandle.H.TupleIsValidHandle(), 0); 
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "trigger_mode", 2);//开启硬触发
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "grab_timeout", -1);
+                HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "trigger_delay_time", lTime*1000); 
+                IntPtr ptr = Marshal.GetFunctionPointerForDelegate(delegateCallback);//取回调函数的地址
+                IntPtr ptr1 = GCHandle.Alloc(test, GCHandleType.Pinned).AddrOfPinnedObject();//取test变量的地址
+                HOperatorSet.SetFramegrabberCallback(hv_AcqHandle, "transfer_end", ptr, ptr1);//注册回调函数
+                button2.Text = "关闭";
+                returnNum();
+                modelList.Enabled = false;
+                btnAddModel.Enabled = false;
+                lazyTime.Enabled = false;
+            }
+            else 
+            {
+                button2.Text = "开启";
+                modelList.Enabled = true;
+                btnAddModel.Enabled = true;
+                lazyTime.Enabled = true;
+                /* string modelName = modelList.SelectedItem.ToString();
+                 int trueNumber = Convert.ToInt32(TrueNum.Text[0]);
+                 int falseNumber = Convert.ToInt32(WrongNum.Text[0]);
+                 int allNumber = Convert.ToInt32(allNum.Text[0]);
+                   chanPin cp = new chanPin();
+                   cp.modelName = modelName;
+                   cp.trueNumber = trueNumber;
+                   cp.falseNumber = falseNumber;
+                   cp.allNumber = allNumber;
+                   
+                DataTable dt = new DataTable();
+                  dt.Columns.Add("模板名");
+                  dt.Columns.Add("相同");
+                  dt.Columns.Add("不同");
+                  dt.Columns.Add("总数");
+                DataRow dr = dt.NewRow();
+                object[] chanPin = { modelName, trueNumber, falseNumber, allNumber };
+                dr.ItemArray=chanPin;
+                dt.Rows.Add(dr);
+
+                Excel::Application xlsApp = new Excel::Application();
+                xlsApp.Workbooks.Add(true);
+                xlsApp.Cells[1][3] = "序号";
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    xlsApp.Cells[i + 2][3] = dt.Columns[i].ColumnName;
+                }
+                xlsApp.Rows[3].HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter;
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    xlsApp.Cells[1][i + 4] = i.ToString();
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        xlsApp.Cells[j + 2][i + 4] = dt.Rows[i][j];
+
+                    }
+                }
+                xlsApp.Rows[4].HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter;
+
+                xlsApp.ActiveWorkbook.SaveAs("F:\\检测产品数据表.xlsx");*/
+            }
+        }
+
+        private void btnAddModel_Click(object sender, EventArgs e)
+        {
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "trigger_mode", 1);
+            HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "grab_timeout", -1);
+            string word = Interaction.InputBox("请输入密码", "身份验证", "", 100, 100);
+            if (word != "123456") { MessageBox.Show("密码错误！"); return; }
+            string str = Interaction.InputBox("请输入模板名字", "创建模板", "", 100, 100);
+            if (str == "") { str = "默认"; };
+            if (false == Directory.Exists("f:modelFiles/model-" + str))
+            {
+              Directory.CreateDirectory("f:modelFiles/model-" + str);
+            }
+            else
+            {
+                MessageBox.Show("已有重复模板！");
+                return;
+            }
+            HObject ho_Image = null, ho_Circle = null, ho_ImageReduced = null;
+            HTuple hv_i = new HTuple();
+            HTuple hv_Width = new HTuple(), hv_Height = new HTuple();
+            HTuple hv_WindowHandle = new HTuple(), hv_Row = new HTuple();
+            HTuple hv_Column = new HTuple(), hv_Radius = new HTuple();
+            HTuple hv_ModelID = new HTuple();
+            // Initialize local and output iconic variables 
+            HOperatorSet.GenEmptyObj(out ho_Image);
+            HOperatorSet.GenEmptyObj(out ho_Circle);
+            HOperatorSet.GenEmptyObj(out ho_ImageReduced);
+            for (hv_i = 1; (int)hv_i <= 4; hv_i = (int)hv_i + 1)
+            {
+                try
+                {                
+                if ((int)hv_i!=1) MessageBox.Show("请将要识别物体顺时针旋转90度，再点击确定");
+                else { MessageBox.Show("单击鼠标左键并拖动选择模板区域，右键确定！");}
+                //HOperatorSet.SetFramegrabberParam(hv_AcqHandle, "software_trig", 1);
+                HOperatorSet.GrabImage(out ho_Image, hv_AcqHandle);
+               // hv_Width.Dispose(); hv_Height.Dispose();
+                HOperatorSet.GetImageSize(ho_Image, out hv_Width, out hv_Height);
+               // HOperatorSet.SetWindowAttr("background_color", "red");
+                HOperatorSet.OpenWindow(0, 0, hv_Width + 1, hv_Height + 1, 0, "visible", "", out hv_WindowHandle);
+                HOperatorSet.DispImage(ho_Image, hv_WindowHandle);
+                HDevWindowStack.Push(hv_WindowHandle);
+                   // ho_Image.Dispose();
+               // hv_Row.Dispose(); hv_Column.Dispose(); hv_Radius.Dispose();
+                HOperatorSet.DrawCircle(hv_WindowHandle, out hv_Row, out hv_Column, out hv_Radius);
+                ho_Circle.Dispose();
+                HOperatorSet.GenCircle(out ho_Circle, hv_Row, hv_Column, hv_Radius);
+                ho_ImageReduced.Dispose();
+                HOperatorSet.ReduceDomain(ho_Image, ho_Circle, out ho_ImageReduced);
+               // hv_ModelID.Dispose();
+                HOperatorSet.CreateShapeModel(ho_ImageReduced, "auto", 0, (new HTuple(360)).TupleRad()
+                    , "auto", "auto", "use_polarity", "auto", "auto", out hv_ModelID);
+                HOperatorSet.WriteShapeModel(hv_ModelID, ("f:modelFiles/model-" + str+"/modle" + hv_i) + ".shm");
+                
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("操作出错！请重新添加模板。"+ex.ToString());
+                    foreach (string file in Directory.GetFileSystemEntries("f:modelFiles/model-" + str))
+                    {
+                        File.Delete(file);
+                    };
+                    Directory.Delete("f:modelFiles/model-" + str);
+                    return;
+                    throw;                   
+                }
+                finally {
+                    HOperatorSet.CloseWindow(hv_WindowHandle);
+                    ho_Image.Dispose();
+                    ho_Circle.Dispose();
+                    ho_ImageReduced.Dispose();
+                    hv_i.UnpinTuple();
+               // hv_i.Dispose();
+               // hv_Width.Dispose();
+                //hv_Height.Dispose();
+                //hv_WindowHandle.Dispose();
+               // hv_Row.Dispose();
+               // hv_Column.Dispose();
+               // hv_Radius.Dispose();
+                //hv_ModelID.Dispose();               
+                }
+            }
+            MessageBox.Show("模板创建成功！");
+            updateList();
+        }
+
+        private void checkModel(HObject ho_Image1)
+        {      
+            HTuple hv_Row = new HTuple();
+            HTuple hv_Column = new HTuple();
+            HTuple hv_Width = new HTuple(), hv_Height = new HTuple();
+            HTuple hv_ModelID1 = new HTuple(), hv_ModelID2 = new HTuple();
+            HTuple hv_ModelID3 = new HTuple(), hv_ModelID4 = new HTuple();
+            HTuple hv_ModelIDs = new HTuple(), hv_Angle = new HTuple();
+            HTuple hv_Score = new HTuple(), hv_ModelIndex = new HTuple();
+
+           // hv_ModelID1.Dispose();
+            HOperatorSet.ReadShapeModel("f:modelFiles/"+modelList.SelectedItem.ToString()+"/modle1.shm", out hv_ModelID1);
+           // hv_ModelID2.Dispose();
+            HOperatorSet.ReadShapeModel("f:modelFiles/" + modelList.SelectedItem.ToString() + "/modle2.shm", out hv_ModelID2);
+            //hv_ModelID3.Dispose();
+            HOperatorSet.ReadShapeModel("f:modelFiles/" + modelList.SelectedItem.ToString() + "/modle3.shm", out hv_ModelID3);
+            //hv_ModelID4.Dispose();
+            HOperatorSet.ReadShapeModel("f:modelFiles/" + modelList.SelectedItem.ToString() + "/modle4.shm", out hv_ModelID4);
+
+           // hv_ModelIDs.Dispose();
+            using (HDevDisposeHelper dh = new HDevDisposeHelper())
+            {
+                hv_ModelIDs = new HTuple();
+                hv_ModelIDs = hv_ModelIDs.TupleConcat(hv_ModelID1);
+                hv_ModelIDs = hv_ModelIDs.TupleConcat(hv_ModelID2);
+                hv_ModelIDs = hv_ModelIDs.TupleConcat(hv_ModelID3);
+                hv_ModelIDs = hv_ModelIDs.TupleConcat(hv_ModelID4);
+            }
+                HOperatorSet.GetImageSize(ho_Image1, out hv_Width, out hv_Height);
+                if (hWindowControl1.HalconWindow.ToString() == "") { return; }
+                HOperatorSet.SetPart(hWindowControl1.HalconWindow, 0, 0, hv_Width+1, hv_Height+1);
+                HOperatorSet.DispObj(ho_Image1, hWindowControl1.HalconWindow);
+               // hv_Row.Dispose(); hv_Column.Dispose(); hv_Angle.Dispose(); hv_Score.Dispose(); hv_ModelIndex.Dispose();
+                HOperatorSet.FindShapeModels(ho_Image1, hv_ModelIDs, 0, (new HTuple(360)).TupleRad()
+                    , 0.5, 8, 0.5, "least_squares", 0, 0.8, out hv_Row, out hv_Column, out hv_Angle,
+                    out hv_Score, out hv_ModelIndex);
+
+                if ((int)(new HTuple(hv_Score.TupleGreater(0))) != 0)
+                {
+                    pictureBox1.BackColor = Color.Green;
+                    a++;
+                    TrueNum.Text = a + "个";
+                }
+                else
+                {
+                    pictureBox1.BackColor = Color.Red;
+                    b++;
+                    WrongNum.Text = b + "个";
+                }
+                c = a + b;
+                allNum.Text = c + "个";
+                /* ho_Image1.Dispose();
+                  hv_AcqHandle.Dispose();
+                  hv_Row.Dispose();
+                  hv_Column.Dispose();
+                  hv_ModelID1.Dispose();
+                  hv_ModelID2.Dispose();
+                  hv_ModelID3.Dispose();
+                  hv_ModelID4.Dispose();
+                  hv_ModelIDs.Dispose();
+                  hv_Angle.Dispose();
+                  hv_Score.Dispose();
+                  hv_ModelIndex.Dispose();*/
+            }
+        }
+   /* public class chanPin
+    {
+        public string modelName { get; set; }
+        public int trueNumber { get; set; }
+        public int falseNumber { get; set; }
+        public int allNumber { get; set; }
+        public DateTime dataTime { get; set; }
+    }*/
+}
